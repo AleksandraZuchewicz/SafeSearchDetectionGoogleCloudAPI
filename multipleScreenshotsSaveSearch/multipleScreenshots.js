e02e9f8c0ed33f30a8cbce6067abe7a44569e619
aleksandra.zuchewicz <aleksandra.zuchewicz@gmail.com>
2020-04-05 11:17:07 +0100

    Added test for multiple screenshots localized in separate folder

---
+++ multipleScreenshotsSaveSearch/multipleScreenshots.js
@@ -0,0 +1,23 @@
+const vision = require("@google-cloud/vision");
+const fs = require("fs");
+
+const TOKEN_ARG = 2;
+const SCREENS_FOLDER_ARG = 3;
+const tokenPath = process.argv[TOKEN_ARG];
+process.env.GOOGLE_APPLICATION_CREDENTIALS = tokenPath;
+const testFolder = process.argv[SCREENS_FOLDER_ARG];
+
+(async function save() {
+    const client = new vision.ImageAnnotatorClient();
+    const files = fs.readdirSync(testFolder);
+    for (const file of files) {
+        const [result] = await client.safeSearchDetection(`${testFolder}/${file}`);
+        const detections = result.safeSearchAnnotation;
+        console.log("Safe search:");
+        console.log(`Adult: ${detections.adult}`);
+        console.log(`Medical: ${detections.medical}`);
+        console.log(`Spoof: ${detections.spoof}`);
+        console.log(`Violence: ${detections.violence}`);
+        console.log(`Racy: ${detections.racy}`);
+    }
+})();
